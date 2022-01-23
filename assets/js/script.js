ed380f9f851291059332be93ab63a73d5ca09f1a
Prolix <51418825+Prolix19@users.noreply.github.com>
2022-01-23 15:28:39 -0600

    Add empty input handling, form reset

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,14 @@ var taskFormHandler = function(event) {
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    // Check if user input is empty
+    if(!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+
+    formEl.reset();
+
     // Package data as an object
     var taskDataObj = {
         name: taskNameInput,
